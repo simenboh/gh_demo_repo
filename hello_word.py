df8e73ca7bc829acab49411591800a9855a59980
SMBE89 <smbe@cowi.com>
2023-12-26 08:13:29 +0100

    Hellow World Hans

---
+++ hello_word.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("Hello, " + name + "! How are you?")
+
+#print("Hello Word, This should be in conflict")
+
---
+++ main.py
@@ -0,0 +1,9 @@
+## TEST MAIN
+
+# importing  all the 
+# functions defined in test.py
+from hello_word import *
+
+greet("Hans")
+
+## dlffd
